148a0578300c68c79746c39c1a849701447a0290
Kyle Shin <kdshin2010@gmail.com>
2024-08-25 22:55:42 -0700

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button variant="contained">Hello world</Button>;
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
